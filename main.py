c25ddac219ea5ed297ce552ea0cb8f065d23559e
amiskkaa <mugavov@mail.ru>
2024-01-09 21:41:51 +0300

    Сделал метод on_click универсальнее (теперь можно функцие передавать аргументы), попробовал использовать кастомные ивенты (помогло избежать ошибку, при завершении игры старым способом)

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ from observers import *
 
 
 def main():
+    QUIT = pygame.USEREVENT + 1
+
     pygame.init()
 
     size = 500, 500
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Название игры")
+    # основной игровой цикл
+    running = True
     # работаем с UI
     main_menu = MainMenu()
-    main_menu.exit_btn.set_click_action(pygame.quit)
+    main_menu.exit_btn.set_click_action(lambda: pygame.event.post(pygame.event.Event(QUIT)))
 
     window_observer = WindowObserver()
     window_observer.set_active_window(main_menu)
-    # основной игровой цикл
-    running = True
     while running:
         screen.fill((0, 0, 0))
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 window_observer.active_window.get_click(event.pos)
